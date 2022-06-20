e73085d2f3f184bbeb0e7136780060656d396508
unknown <Kyazimov123@gmail.com>
2022-06-20 18:15:48 +0300

    second commit

---
+++ Zachet/Radio.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zachet
+{
+    public class Radio
+    {
+        private int channel;
+        private int volume;
+        public Radio()
+        {
+            channel = 0;
+            volume = 1;
+        }
+        public void CH_Next()
+        {
+            if (channel == 9)
+            {
+                channel = 0;
+            }
+            else
+                channel++;
+        }
+        public void CH_Prriv()
+        {
+            if (channel == 0)
+            {
+                channel = 9;
+            }
+            else
+                channel--;
+        }
+        public void Vol_Next()
+        {
+            if (volume < 9)
+            {
+                volume++;
+            }
+        }
+        public void Vol_Priv()
+        {
+            if (volume > 0)
+            {
+                volume--;
+            }
+        }
+        public int Channel
+        {
+            set
+            {
+                if ((value <= 10) && (value >= 0))
+                {
+                    channel = value;
+                }
+                else
+                    Console.WriteLine("Канал должен быть в диапазоне от 0 до 10");
+            }
+            get { return channel; }
+
+        }
+
+        public int Volume
+        {
+            set
+            {
+                if ((value <= 10) && (value >= 0))
+                {
+                    volume = value;
+                }
+                else
+                    Console.WriteLine("Канал должен быть в диапазоне от 0 до 10");
+            }
+            get { return volume; }
+
+        }
+    }
+}
